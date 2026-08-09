881e6f9f96cdcd0ad6a7adf44b78cc941296b9e1
afzal ansari <afzalanasari076@gmail.com>
2026-08-09 19:12:47 +0530

    Get all order sir

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -33,3 +33,55 @@ exports.newOrder = catchAsyncErrors(async (req, res, next) => {
     });
 
 });
+
+// get Single order -- for logged in user it will give name and email of user
+exports.getSingleOrder = catchAsyncErrors(async (req,res,next)=>{
+
+    const order = await Order.findById(req.params.id).populate("user","name email");
+     //In ordermodel we have userid 
+    //from that userid it will take name and email from user database by comparing both user id
+
+    if(!order){
+        return next(new ErrorHander("Order not found with this id ",404));
+    }
+
+    res.status(200).json({
+        success:true,
+        order,
+    })
+
+});
+
+// get single Order -- for logged in user
+
+exports.myOrders = catchAsyncErrors(async (req,res,next)=>{
+
+    const orders = await Order.find({user: req.user._id});//user id from loggin person
+  
+
+    res.status(200).json({
+        success:true,
+        orders,
+    })
+
+});
+
+//get All order -- For admin
+exports.getAllOrders = catchAsyncErrors(async (req,res,next)=>{
+
+    const orders = await Order.find();// return all Orders and data of Order table/schema
+
+    let totalAmount = 0; // like we have 10 orders so we calculate total amount of all orders
+    orders.forEach(order=>{
+        totalAmount+= order.totalPrice;
+    });
+  
+
+    res.status(200).json({
+        success:true,
+        totalAmount,
+        orders,
+    })
+
+});
+
